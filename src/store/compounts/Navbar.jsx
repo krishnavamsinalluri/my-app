9bbcc00a73edbd4364dc9a6e360c9e0788503c51
krishnavamsinalluri <vamsinalluri806@gmail.com>
2024-02-21 15:46:40 +0530

    sss

---
+++ src/store/compounts/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+                    <div>
+                <nav class="navbar bg-primary">
+                <div class="container-fluid">
+                    <h3 class="navbar-brand mb-0 h1 bi bi-house-fill" >home</h3>
+                    <h3 class="navbar-brand mb-0 h1 bi bi-house-fill" >home</h3>
+
+                </div>
+                </nav>
+            </div>
+ 
+    </div>
+
+  )
+}
+
+export default Navbar
