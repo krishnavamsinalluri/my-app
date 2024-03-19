b91c1ef78767ab5d0770d81a826bb3c75c87869b
krishnavamsinalluri <vamsinalluri806@gmail.com>
2024-03-19 19:48:35 +0530

    onlines

--- src/store/compounts/banner.jsx
+++ src/store/compounts/banner.jsx
@@ -16,6 +16,7 @@ function Banner() {
     <div class="carousel-item">
       <img src="https://img.freepik.com/premium-vector/product-advertising-hero-image-header-layout_1302-21013.jpg?size=626&ext=jpg&ga=GA1.1.2126216559.1699334969&semt=ais" className="d-block w-100 h-300 shadow-lg" height='300' alt="..."/>
     </div>
+    
     <div class="carousel-item">
       <img src="https://i.pinimg.com/736x/87/48/17/874817126d4eb4a6847cd4b3d290b65d.jpg" className="d-block w-100 h-300 shadow-lg" height='300' alt="..."/>
     </div>
