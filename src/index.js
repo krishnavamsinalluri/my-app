96e36e6d510c184ed796da831604e101a450a0d3
krishnavamsinalluri <vamsinalluri806@gmail.com>
2024-03-20 15:36:34 +0530

    onlines

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { CartProvider } from './store/context/Cartcontext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={`/my-app`}>
+  <BrowserRouter >
     <React.StrictMode>
       <CartProvider>  
           <App />
